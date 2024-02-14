b9801c3e127aa3358d866a9b7ace160ae1947ed5
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2024-02-14 11:13:43 +0500

    some data type added

---
+++ d.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a= 45;
+    short b=66;
+    float c=45.3;
+
+    cout<< a << endl;
+    cout<< b << endl;
+    cout<< c << endl;
+}
