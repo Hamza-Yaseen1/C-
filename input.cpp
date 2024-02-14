aefad4f093b7baf5a5314c1a2758c0bebc635e86
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2024-02-14 11:46:29 +0500

    Added subtraction and multiplication operations to the program.

--- input.cpp
+++ input.cpp
@@ -9,7 +9,10 @@ int main(){
     cout<<"Enter the number b"<<endl;
     cin>>b;
 
-   cout<<"The sum of a + b:"<<a + b<<endl;
-   cout<<"The sum of a / b:"<<(float)a / b<<endl; //typecasting did in this to get accurate output
+   cout<<"The sum of a + b: "<<a + b<<endl;
+   cout<<"The sum of a - b: "<<a - b<<endl;
+   cout<<"The sum of a * b: "<<a * b<<endl;
+
+   cout<<"The sum of a / b: "<<(float)a / b<<endl; //typecasting did in this to get accurate output
     return 0;
 }
