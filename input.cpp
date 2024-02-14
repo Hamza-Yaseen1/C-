fb1d9b5027a7182090341202ab1c812e5cf2bd3d
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2024-02-14 11:34:47 +0500

    take input and sum the input

---
+++ input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+
+    cout<<"Enter the number a"<<endl;
+    cin>>a;
+    cout<<"Enter the number b"<<endl;
+    cin>>b;
+
+   cout<<"The sum of a + b:"<<a + b;
+    return 0;
+}
