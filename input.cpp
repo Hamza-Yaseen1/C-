dffaa08cebbe9505a1da9b501d562c01ae3da3a7
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2024-02-14 11:44:03 +0500

    typecasting used

--- input.cpp
+++ input.cpp
@@ -9,6 +9,7 @@ int main(){
     cout<<"Enter the number b"<<endl;
     cin>>b;
 
-   cout<<"The sum of a + b:"<<a + b;
+   cout<<"The sum of a + b:"<<a + b<<endl;
+   cout<<"The sum of a / b:"<<(float)a / b<<endl; //typecasting did in this to get accurate output
     return 0;
 }
