4379ccdb111d8813403def80412aba6b6acddf0e
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2024-02-21 11:26:51 +0500

    loops

---
+++ loop.cpp
@@ -0,0 +1,15 @@
+//loops
+#include <iostream>
+using namespace std;
+
+int main(){
+    int line =0;
+//While loop
+    while(line<30){
+
+    cout << "We are at line number : "<<line<<endl;
+
+    line= line + 1;
+    }
+    return 0;
+}
